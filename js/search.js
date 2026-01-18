b7e9910087993fe2657d7a91d9d98a160dce7b9f
Dutchhalogen <samuel.fluor@gmx.ch>
2026-01-18 16:48:52 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -35,13 +35,13 @@ function searchSite(query) {
       resultsContainer.innerHTML += `
         <div class="list-group-item mb-3 p-3 shadow-sm rounded d-flex flex-column flex-md-row justify-content-between align-items-start">
           <div class="me-3">
-            <h5>Title</h5>
-            <p>Snippet...</p>
+            <h5>${item.title}</h5>
+            <p>${snippet}</p>
           </div>
-          <a href="#" class="btn btn-outline-primary btn-sm mt-2 mt-md-0">Zum Treffer</a>
+          <a href="${item.url}" class="btn btn-primary btn-sm mt-2 mt-md-0">Zum Treffer</a>
         </div>
       `;
-
+      
       startIndex += query.length;
       totalResults++;
     }
