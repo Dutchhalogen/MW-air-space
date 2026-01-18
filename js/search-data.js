38bdc73a1cfb1b75cc6852b34655d617680aca8e
Dutchhalogen <samuel.fluor@gmx.ch>
2026-01-18 15:54:04 +0100

    Refactor Stealth content for clarity and structure

--- js/search-data.js
+++ js/search-data.js
@@ -9,11 +9,9 @@ const searchIndex = [
     title: "Stealth – Einführung",
     url: "stealth.html#intro",
     content: "Stealth spielt in der modernen Kriegsführung eine sehr wichtige Rolle. Sie ist nicht nur äusserst vielseitig, sondern es gibt auch viele verschiedene Varianten, sodass sich für praktisch jede Mission eine anwendbare Stealth Technologie finden lässt. Im Allgemeinen ist Stealth jedoch ein simples Konzept. Übersetzt man das Wort "Stealth" ins Deutsche, erhält man das Wort "Heimlichkeit", was das Prinzip eigentlich ziemlich akkurat wiedergibt. 
-
-Im Grunde ist Stealth eine defensive Eigenschaft. Aber anstatt Treffer durch umfangreiche Panzerung zu überstehen oder durch aktive defensive Systeme Angriffe zu verhindern, benutzt Stealth die simpelste und älteste Defensivtaktik: Nicht gesehen werden. Dies ist bei jeder Art von Stealth ausnahmslos der Fall. Worin sie sich unterscheiden, liegt in dem Bereich, indem sie operieren. 
-
-Einige dieser Stealth arten werden schon seit Jahrtausenden verwendet, ersichtlich in Sun Tzu’s «The Art of War»( 5. Jahrhundert v. Chr.) worin geschrieben steht «All warfare is based on deception.», womit besagt wird das jeder Kampf auf Täuschung basiert ist. Ein weiteres Beispiel ist "Visual Stealth", die beschreibt wie man sich vor den Augen des Feindes tarnt. Solche Taktiken gibt es schon seit Tausenden von Jahren. Andere Taktiken sind jedoch sehr modern, wie zum Beispiel Radar- oder Infrarot Stealth, die es erst seit den 1980er Jahren gibt. 
-Die wichtigsten Stealth Arten sind unten erklärt."
+    Im Grunde ist Stealth eine defensive Eigenschaft. Aber anstatt Treffer durch umfangreiche Panzerung zu überstehen oder durch aktive defensive Systeme Angriffe zu verhindern, benutzt Stealth die simpelste und älteste Defensivtaktik: Nicht gesehen werden. Dies ist bei jeder Art von Stealth ausnahmslos der Fall. Worin sie sich unterscheiden, liegt in dem Bereich, indem sie operieren. 
+    Einige dieser Stealth arten werden schon seit Jahrtausenden verwendet, ersichtlich in Sun Tzu’s «The Art of War»( 5. Jahrhundert v. Chr.) worin geschrieben steht «All warfare is based on deception.», womit besagt wird das jeder Kampf auf Täuschung basiert ist. Ein weiteres Beispiel ist "Visual Stealth", die beschreibt wie man sich vor den Augen des Feindes tarnt. Solche Taktiken gibt es schon seit Tausenden von Jahren. Andere Taktiken sind jedoch sehr modern, wie zum Beispiel Radar- oder Infrarot Stealth, die es erst seit den 1980er Jahren gibt. 
+    Die wichtigsten Stealth Arten sind unten erklärt."
   },
   {
     title: "Visual Stealth",
