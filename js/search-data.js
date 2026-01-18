869dc5feaba45273e95447a3acd09288c0bc1297
Dutchhalogen <samuel.fluor@gmx.ch>
2026-01-18 16:10:26 +0100

    Update search-data.js

--- js/search-data.js
+++ js/search-data.js
@@ -3,7 +3,7 @@ const searchIndex = [
   {
     title: "Home",
     url: "index.html#home",
-    content: "Hier kommt der Text von deiner Startseite rein."
+    content: `Hier kommt der Text von deiner Startseite rein.`
   },
   {
     title: "Stealth – Einführung",
@@ -16,27 +16,27 @@ const searchIndex = [
   {
     title: "Visual Stealth",
     url: "stealth.html#visual",
-    content: 'Hier kommt dein Visual Stealth Text rein.'
+    content: `Hier kommt dein Visual Stealth Text rein.`
   },
   {
     title: "Radar Stealth",
     url: "stealth.html#radar",
-    content: 'Hier kommt dein Radar Stealth Text rein.'
+    content: `Hier kommt dein Radar Stealth Text rein.`
   },
   {
     title: "Infrared Stealth",
     url: "stealth.html#infrared",
-    content: "Hier kommt dein Infrared Stealth Text rein."
+    content: `Hier kommt dein Infrared Stealth Text rein.`
   },
   {
     title: "Sonar Stealth",
     url: "stealth.html#sonar",
-    content: "Hier kommt dein Sonar Stealth Text rein."
+    content: `Hier kommt dein Sonar Stealth Text rein.`
   },
   {
     title: "Sensor Fusion",
     url: "sensor_fusion.html#fusion",
-    content: "Hier kommt dein Text zu Sensor Fusion rein."
+    content: `Hier kommt dein Text zu Sensor Fusion rein.`
   }
   // weitere Seiten / Kapitel kannst du hier hinzufügen
 ];
